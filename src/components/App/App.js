da7118c8cfe046b1cef0b2f3758eaace34f69e86
Дмитрий Цебинога <tsebik@ukr.net>
2022-09-22 16:18:51 +0300

    filter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,17 +21,51 @@ class App extends Component {
     }));
   };
 
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  getFiltredContacts = () => {
+    const { contacts, filter } = this.state;
+
+    if (!filter) {
+      return contacts;
+    }
+
+    const normalisedFilter = filter.toLowerCase();
+    const filteredContacts = contacts.filter(({ name }) => {
+      const normalisedName = name.toLowerCase();
+      const result = normalisedName.includes(normalisedFilter);
+
+      return result;
+    });
+
+    return filteredContacts;
+  };
+
   render() {
-    const { contacts } = this.state;
+    const { filter } = this.state;
+    const { addContacts, handleChange } = this;
+    const contacts = this.getFiltredContacts();
 
     return (
       <Div>
         <h1>Phonebook</h1>
 
-        <Form onSubmit={this.addContacts} />
+        <Form onSubmit={addContacts} />
 
         <div>
           <h2>Contacts</h2>
+
+          <p>Find contacts by name</p>
+          <input
+            type="text"
+            name="filter"
+            value={filter}
+            onChange={handleChange}
+          />
+
           <ul>
             <Contacts contacts={contacts} />
           </ul>
